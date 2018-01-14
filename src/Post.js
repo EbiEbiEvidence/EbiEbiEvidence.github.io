af8196710aee6929c4c7b1edd980774f4d47bc28
KilledByNLP <iwasa@katfuji.lab.tuat.ac.jp>
2018-01-15 01:08:45 +0900

    Add Sharing Buttons

--- src/Post.js
+++ src/Post.js
@@ -6,6 +6,8 @@ import './Post.css'
 
 class Post extends Component {
   state = {
+    title: '',
+    date: '',
     body: ''
   }
 
@@ -18,7 +20,9 @@ class Post extends Component {
       })
       .then(response => {
         this.setState({
-          body: response
+          title: response.split(/\n/)[0].slice(1),
+          date: response.split(/\n/)[1],
+          body: response.split(/\n/).slice(2).join("\n"),
         })
         document.title = response.split(/\n/)[0].slice(1)
       })
@@ -30,11 +34,25 @@ class Post extends Component {
   }
 
   render() {
-    const { body } = this.state
+    const { title, body, date } = this.state
 
     return (
       <Container>
         <div className='article'>
+          <h1>{ title }</h1>
+          <h3 style={{textAlign: 'right'}}><i>Posted on { date }</i></h3>
+          <div className='shares'>
+            <a className="twitter-share-button"
+              href="https://twitter.com/share"
+              data-size="large"
+              data-size="large"
+              data-lang="ja"
+              data-dnt="true">
+            Tweet
+            </a>
+            <a href="http://b.hatena.ne.jp/entry/" className="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style={{border: 'none'}} /></a>
+            <div className="fb-like"　data-layout="box_count"></div>
+          </div>
           {processor.processSync(body).contents}
         </div>
       </Container>
