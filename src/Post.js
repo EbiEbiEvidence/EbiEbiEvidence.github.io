3b09488b3ce8ed82ec5ef68082b34495451b7266
KilledByNLP <iwasa@katfuji.lab.tuat.ac.jp>
2018-01-15 19:53:39 +0900

    (*^^*)

--- src/Post.js
+++ src/Post.js
@@ -14,7 +14,7 @@ class Post extends Component {
   componentDidMount = () => {
     const { id } = this.props.match.params
     if (!isNaN(id)) {
-      fetch('/post-sources/' + id + '.md')
+      fetch('/posts/' + id + '.md')
       .then(response => {
         return response.text()
       })
